c04f081654762f3d852f1dbe3cf7f801e0661cda
Soumyadip Pal <arka.pal21@gmail.com>
2025-01-17 11:15:31 +0530

    Updated images and UI functionality

--- GameProject/NewProject.cs
+++ GameProject/NewProject.cs
@@ -19,37 +19,46 @@ namespace StrayEditor.GameProject
         public string ProjectFile { get; set; }
         [DataMember]
         public List<string> Folders { get; set; }
+        public byte[] Icon { get; set; }
+
+        public byte[] Screenshot { get; set; }
+
+        public string IconFilePath { get; set; }
+
+        public string ScreenShotFilePath { get; set; }
+
+        public string  ProjectFilePath { get; set; }
     }
     class NewProject : ViewModelBase
     {
         // TODO : get path from installation location
         private readonly string _templatePath = @"..\..\StrayEditor\Project Templates";
-        private string _name = "New Project";
+        private string _projectName = "New Project";
 
-        public string Name
+        public string ProjectName
         {
-            get => _name;
+            get => _projectName;
             set
             {
-                if (_name != value)
+                if (_projectName != value)
                 {
-                    _name = value;
-                    OnPropertyChanged(nameof(Name));
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
                 }
             }
         }
 
-        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\StrayProject\";
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\StrayProject\";
 
-        public string Path
+        public string ProjectPath
         {
-            get => _path;
+            get => _projectPath;
             set
             {
-                if (_path != value)
+                if (_projectPath != value)
                 {
-                    _path = value;
-                    OnPropertyChanged(nameof(Path));
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
                 }
             }
         }
@@ -67,6 +76,11 @@ namespace StrayEditor.GameProject
                 foreach (var file in templateFiles)
                 {
                     var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenShotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenShotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
                     projectTemplates.Add(template);
                 }
             }
